e00aac7660c57fd2ebfc215e9ce59006d5e3a001
squall2046 <squall2046@hotmail.com>
2019-04-12 11:38:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("client/public"));
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
-const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost/googlebooks"
+const mongoURL = process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
 mongoose.connect(mongoURL, {useNewUrlParser: true})
   .then(() => {
     console.log("🌎 ==> Successfully connected to mongoDB.");
